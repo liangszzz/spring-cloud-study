a4487025a615fcbb2b1037a44cbbbab87cb21638
liang <liang-shan@outlook.com>
2019-11-08 16:56:16 +0800

    spring cloud config nacos

--- order-service/src/main/java/com/github/ls/order/controller/Controller.java
+++ order-service/src/main/java/com/github/ls/order/controller/Controller.java
@@ -1,13 +1,19 @@
 package com.github.ls.order.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class Controller {
 
+    @Value("${abc:}")
+    private String abb;
+
     @GetMapping("/index")
     public String index() {
-        return "hello gateway";
+        return "hello " + abb;
     }
 }
