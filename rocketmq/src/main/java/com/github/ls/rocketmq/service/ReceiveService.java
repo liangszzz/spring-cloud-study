0d2dc961891f757baa13bd9ff729255b71512361
liang <liang-shan@outlook.com>
2019-11-12 23:49:41 +0800

    stream

--- rocketmq/src/main/java/com/github/ls/rocketmq/service/ReceiveService.java
+++ rocketmq/src/main/java/com/github/ls/rocketmq/service/ReceiveService.java
@@ -1,17 +1,16 @@
 package com.github.ls.rocketmq.service;
 
-import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.stereotype.Component;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.stereotype.Service;
 
+@Service
+@EnableBinding(Sink.class)
 public class ReceiveService {
 
-    /**
-     * 默认是input，在Sink类中指定，如果想要多个input，需要写一个实现Sink的类
-     *
-     * @param receiveMsg
-     */
+
+    @StreamListener(Sink.INPUT)
     public void receiveInput1(String receiveMsg) {
         System.out.println("input receive: " + receiveMsg);
     }
