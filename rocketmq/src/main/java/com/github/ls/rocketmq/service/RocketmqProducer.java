7e14a0ddff73dcdf069bbd8c0d38ea7c1ae91e40
liang <liang-shan@outlook.com>
2019-11-12 18:20:28 +0800

    rocketmq

--- rocketmq/src/main/java/com/github/ls/rocketmq/service/RocketmqProducer.java
+++ rocketmq/src/main/java/com/github/ls/rocketmq/service/RocketmqProducer.java
@@ -1,27 +1,27 @@
 package com.github.ls.rocketmq.service;
 
-import org.apache.rocketmq.client.exception.MQBrokerException;
-import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.client.producer.DefaultMQProducer;
-import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableBinding(Source.class)
 public class RocketmqProducer {
 
-    DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name_4");
+    @Autowired
+    private Source source;
 
-    public void send(String message) throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
-        producer.setNamesrvAddr("localhost:9876");
-        try {
-            producer.start();
-        }
-        catch (Exception e){
-
-        }
-        Message msg = new Message("TopicCreate1", "TagB", message.getBytes());
-        producer.send(msg);
+    public void send(String message) {
+        MessageBuilder builder = MessageBuilder.withPayload(message)
+                .setHeader(RocketMQHeaders.TAGS, "binder")
+                .setHeader(RocketMQHeaders.KEYS, "my-key")
+                .setHeader("DELAY", "1");
+        Message msg = builder.build();
+        source.output().send(msg);
     }
 
 }
